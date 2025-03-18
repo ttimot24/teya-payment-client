e6172498237805916ed884bc0342cf9443a24aad
Timot Tarjani <ttimotster@gmail.com>
2025-03-19 00:06:40 +0100

    Replace transactionId in test

--- tests/TeyaApiClientTest.php
+++ tests/TeyaApiClientTest.php
@@ -22,14 +22,14 @@ class TeyaApiClientTest extends TestCase {
 
         $response = $client->payment($payment);
         $this->assertEquals(201, $response->getStatusCode());
-        $this->assertEquals('{"id":1,"status":"success"}', $response->getBody()->getContents());
+        $this->assertEquals('{"TransactionId":"tr_29yag5DnGmMAsl-F79Xiomz2UxPpaX_g","TransactionType":"Sale","Amount":100,"Currency":"352","TransactionDate":"2025-03-18T22:51:36+00:00","OrderId":"ORDER1230001","AuthCode":"082996","ActionCode":"000","TransactionStatus":"Accepted","PaymentMethod":{"PaymentType":"Card","PAN":"417666******0104","ExpYear":"2031","ExpMonth":"12","CardType":"Visa"}}', $response->getBody()->getContents());
     }
 
     public function testGetTransaction(){
 
         $client = new Ttimot24\TeyaPayment\TeyaApiClient(['PrivateKey' => '856293_pr0lxnW8PG1SeCwVJ3WPH0lXCeU0/sYLtX', 'log_enabled' => true, 'log_level' => 'debug']);
-        $response = $client->transaction('ORDER1230001');
-        $this->assertEquals('{"id":1,"status":"success"}', $response);
+        $response = $client->transaction('tr_29yag5DnGmMAsl-F79Xiomz2UxPpaX_g');
+        $this->assertEquals('{"id":1,"status":"success"}', $response->getBody()->getContents());
     }
 
 }
