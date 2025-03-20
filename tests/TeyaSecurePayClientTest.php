307825ea636cc6d988605e3b34e4c1bcd3ed5ec6
Timot Tarjani <ttimotster@gmail.com>
2025-03-20 18:27:45 +0100

    OrderHash validation partial setup

--- tests/TeyaSecurePayClientTest.php
+++ tests/TeyaSecurePayClientTest.php
@@ -40,6 +40,26 @@ class TeyaSecurePayClientTest extends TestCase {
         $this->assertEquals("ef2e66e64df91143e7e98ecc9f94e12988718408b860770b4181e466401f22d0", $checkHash);
     }
 
+    public function testSignatureValidation(){
+
+        $signatureClient = new Ttimot24\TeyaPayment\TeyaSecurePayClient([
+            'MerchantId' => '9123456', 
+            'PaymentGatewayId' => 16, 
+            'SecretKey' => '1234567890abcdef',
+            'RedirectSuccess' => 'https://borgun.is/success',
+            'RedirectSuccessServer' => 'https://borgun.is/success_server'
+        ]);
+
+
+        $validation = $signatureClient->validateSignature([
+            "amount" => 100,
+            "currency" => "ISK",
+            "orderid" => "TEST00000001"
+        ]);
+
+        $this->assertTrue($validation);
+    }
+
     public function testStartTransaction(){
 
         $this->client->addItems([
